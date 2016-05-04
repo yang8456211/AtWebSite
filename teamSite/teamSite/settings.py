2798cd1141a8df62132d0520f4bcee2e32cdb45e
90后不后 <347702498@qq.com>
2016-05-04 21:56:16 +0800

    更换数据库,使用mysql

--- teamSite/teamSite/settings.py
+++ teamSite/teamSite/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'teamSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'teamWeb',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
